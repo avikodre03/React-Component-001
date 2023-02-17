8be2f9a71ba5330c10b3707ed3b0b01c0099e364
avikodre03 <akodre111@gmail.com>
2023-02-17 23:55:11 +0530

    resume

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+function Header(){
+    return(
+        <div className="headercontainer">
+            <h1>Avinash Kodre</h1>
+            <p> Full Stack Developer</p>
+            <div><a href="https://www.linkedin.com/in/avinash-kodre/">Linkedin</a> <a href="https://github.com/avikodre03">Github</a></div>
+            <hr />
+        </div>
+    )
+}
+export default Header;
